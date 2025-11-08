da3869c9c786df3ecda07acbcec10772968b2a37
lokeshlavate56-abcd <lokeshlavate56@gmail.com>
2025-11-08 15:52:30 +0530

    Add files via upload

---
+++ exp-2b.c
@@ -0,0 +1,22 @@
+// Lokesh Lavate,UIN:-251P083,YEAR:-F.E,DIV.D
+#include <stdio.h>
+
+int main() {
+    int start, end, sum = 0;
+
+    printf("Enter the starting number: ");
+    scanf("%d", &start);
+
+    printf("Enter the ending number: ");
+    scanf("%d", &end);
+
+    for (int i = start; i <= end; i++) {
+        if (i % 2 != 0) {  
+            sum += i;
+        }
+    }
+
+    printf("Sum of all odd numbers between %d and %d is: %d\n", start, end, sum);
+
+    return 0;
+}
